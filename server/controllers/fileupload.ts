5260a6b08d175f6bcb48c3f689d01dd54156e3df
Milad Ahmadi <you@example.com>
2020-07-08 07:50:21 +0430

    build

--- server/controllers/fileupload.ts
+++ server/controllers/fileupload.ts
@@ -9,7 +9,7 @@ var Throttle = require("stream-throttle").Throttle;
 export default class UploadCtrl extends BaseCtrl{
   model=Image
   upload = (req, res) => {
-    const fileName = `sanay_${Date.now()}.jpg`;
+    const fileName = `kairan_${Date.now()}.jpg`;
     try {
         // process.env.IMAGE_UPLOAD_DIR
 
