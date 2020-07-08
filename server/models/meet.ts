3315c0030e1fb7055c5dd4d643a9b24d6c7fe402
Milad Ahmadi <you@example.com>
2020-07-09 02:18:35 +0430

    build

--- server/models/meet.ts
+++ server/models/meet.ts
@@ -3,17 +3,20 @@ import moment = require("moment-timezone");
 import * as mongoosePaginate from 'mongoose-paginate-v2';
 
 const meetSchema = new mongoose.Schema({
-  category:String,
-  user:String,
-  day:[String],//{type:String,enum:['sun','mon','tue','wed','thu','fri','sat'],default:'sun'},
-  startTime:Number,
-  endTime:Number,
-  description:String,
-  price:Number,
-  per:{type:String,enum:['night','day','hour'],default:'hour'},
-  is_active: {type: Boolean,  default: true},
+  category: String,
+  user: String,
+  description: String,
+  images: [String],
+  timeSheets: [{
+    day: [String],//{type:String,enum:['sun','mon','tue','wed','thu','fri','sat'],default:'sun'},
+    startTime: String,
+    endTime: String,
+    price: Number,
+    per: { type: String, enum: ['night', 'day', 'hour'], default: 'hour' },
+  }],
 
-  deleted:{type:Boolean, default:false},
+  is_active: { type: Boolean, default: true },
+  deleted: { type: Boolean, default: false },
   created_at: { type: Number, "default": moment().unix() },
   updated_at: { type: Number, "default": moment().unix() }
 });
